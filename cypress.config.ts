1194c40968e84bf39b86ea1b6a9d7438264bfb42
ur-valkyrie <castiel.andreeva@gmail.com>
2023-10-30 00:30:14 +0100

    added new file 'firstTest.cy.js' and updated configs

---
+++ cypress.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:4200/pages",
+    watchForFileChanges: true,
+    retries: {
+      openMode: 1,
+      runMode: 0
+    },
+    defaultCommandTimeout: 1000,
+    pageLoadTimeout: 20000,
+    screenshotsFolder: "cypress/cypress_screenshot",
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
