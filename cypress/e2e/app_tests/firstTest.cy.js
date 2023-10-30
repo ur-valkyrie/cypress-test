cf8a3a88aa7fb961fc7d923b4ff3b8d09ff0ceb2
ur-valkyrie <83160853+ur-valkyrie@users.noreply.github.com>
2023-10-30 17:03:10 +0100

    Update firstTest.cy.js

--- cypress/e2e/app_tests/firstTest.cy.js
+++ cypress/e2e/app_tests/firstTest.cy.js
@@ -1,13 +1,13 @@
 describe("My First Test", () => {
     it("Checks the Form Layouts page", () => {
       cy.visit("/forms/layouts");
-      cy.url().should('contain', '/forms/layouts')
-      cy.get(".text").should("contain.text", "Option 1")
+      cy.url().should("contain", "/forms/layouts");
+      cy.get(".text").should("contain.text", "Option 1");
     });
   
     it("Checks the Window page", () => {
       cy.visit("//modal-overlays/window");
-      cy.url().should('contain', '/modal-overlays/window')
-      cy.get(".ng-star-inserted").should("contain.text", "Window Form")
+      cy.url().should("contain", "/modal-overlays/window");
+      cy.get(".ng-star-inserted").should("contain.text", "Window Form");
     });
   });
