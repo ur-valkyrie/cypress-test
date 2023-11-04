287732f530d3bcba777a24adc0f6febff3fb00ed
ur-valkyrie <castiel.andreeva@gmail.com>
2023-11-04 20:29:12 +0100

    Updated files'

--- cypress/e2e/app_tests/stepperTests.cy.js
+++ cypress/e2e/app_tests/stepperTests.cy.js
@@ -1,13 +1,11 @@
 describe('Form Layouts Test', () => {
     it('Checks the Form Layouts page', () => {
       cy.visit('/pages/layout/stepper');
-      cy.get('.step-content').first().should('include.text', 'Step content #1');
-      cy.contains('next').first().click();
-      cy.get('.step-content').first().should('include.text', 'Step content #2');
-      cy.contains('next').first().click();
-      cy.get('.step-content').first().should('include.text', 'Step content #3');
-      cy.contains('next').first().click();
-      cy.get('.step-content').first().should('include.text', 'Step content #4');
-      cy.contains('next').first().should('be.disabled');
+      const arrEl = ['Step content #1', 'Step content #2', 'Step content #3', 'Step content #4'];
+      arrEl.forEach(el => {
+          cy.get('.step-content').first().should('include.text', el);
+          el === 'Step content #4' ? cy.contains('next').first().should('be.disabled')
+          :cy.contains('next').first().click();
+      })
     });
   });
